ac932819d42bfda976d1edf2e108d2f708e6e9bc
Jeroimo <jeronimoriveros@gmail.com>
2024-08-21 22:00:17 -0500

    ALU op 4 bits

--- components/ALU.v
+++ components/ALU.v
@@ -1,29 +1,36 @@
 module ALU(
     input wire [31:0] a,    // Operando A
     input wire [31:0] b,    // Operando B
-    input wire [2:0] op,    // Código de operación
+    input wire [3:0] ALU_op,    // Código de operación ALU de 4 bits
     output reg [31:0] result, // Resultado
     output wire zero        // Bandera de cero
 );
 
     // Operaciones: 
-    // 000 - Suma
-    // 001 - Resta
-    // 010 - AND
-    // 011 - OR
-    // 100 - XOR
-    // 101 - NOR
-    // 110 - Operación no definida
-    // 111 - Operación no definida
+    // 0000 - Suma
+    // 0001 - Resta
+    // 0010 - A < B
+    // 0011 - A < B (unsigned)
+    // 0100 - AND
+    // 0101 - OR
+    // 0110 - XOR
+    // 0111 - NOR
+    // 1100 - Corrimiento a la derecha
+    // 1101 - Corrimiento a la izquierda
+ 
 
     always @(*) begin
-        case (op)
-            3'b000: result = a + b;    // Suma
-            3'b001: result = a - b;    // Resta
-            3'b010: result = a & b;    // AND
-            3'b011: result = a | b;    // OR
-            3'b100: result = a ^ b;    // XOR
-            3'b101: result = ~(a | b); // NOR
+        case (ALU_op)
+            4'b0000: result = a + b;    // Suma
+            4'b0001: result = a - b;    // Resta
+            4'b0010: result = (a < b) ? 1 : 0;    // A < B
+            4'b0011: result = (a < b) ? 1 : 0;    // A < B (unsigned)
+            4'b0100: result = a & b;    // AND
+            4'b0101: result = a | b;    // OR
+            4'b0110: result = a ^ b;    // XOR
+            4'b0111: result = ~(a | b); // NOR
+            4'b1100: result = a >> 1;   // Corrimiento a la derecha
+            4'b1101: result = a << 1;   // Corrimiento a la izquierda
             default: result = 32'h00000000; // Operación no definida
         endcase
     end
